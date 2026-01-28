31c23c921f02a4dda72e9a356215e3856139ed3e
Vikrantvyas <gotovikrant@gmail.com>
2026-01-28 10:43:20 +0530

    Initial website under construction

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+      <h1>English & Computer Institute</h1>
+      <p>Spoken English | Computer Courses | Tally</p>
+
+      <p>📍 Indore</p>
+      <p>📞 Contact: 9XXXXXXXXX</p>
+
+      <p style={{ color: "green", marginTop: "20px" }}>
+        Website Under Construction 🚧
+      </p>
+    </main>
+  );
+}
