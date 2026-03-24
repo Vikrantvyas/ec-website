b4df1524c98f94ebcc99ab6bfb3a26299c829196
Vikrantvyas <gotovikrant@gmail.com>
2026-03-24 09:54:00 +0530

    branch_id standardization + receipt UI + due logic fix

--- app/components/admin/attendance/AttendanceMain.tsx
+++ app/components/admin/attendance/AttendanceMain.tsx
@@ -189,7 +189,7 @@ export default function AttendanceMain({
               {isExpanded && (
                 <StudentActions 
                   studentId={student.id} 
-                  branchId={selectedBranch}
+                  
                 />
               )}
 
