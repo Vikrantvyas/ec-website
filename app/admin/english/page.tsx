f858be4761bb5672dd0c49ff4ae318069c5c2f93
Vikrantvyas <gotovikrant@gmail.com>
2026-08-30 11:31:06 +0530

    sync image board navigation with left panel

--- app/admin/english/page.tsx
+++ app/admin/english/page.tsx
@@ -306,9 +306,8 @@ export default function EnglishPage() {
         selectedGrammarTableId={selectedGrammarTableId}
         setSelectedGrammarTableId={setSelectedGrammarTableId}
         selectedImageId={selectedImageId}
-        
-
         setSelectedImageId={setSelectedImageId}
+                
       />
       <div className="flex-1 flex flex-col items-center pt-4 gap-2">
 
@@ -352,6 +351,7 @@ export default function EnglishPage() {
             selectedImageId={selectedImageId}
             showImages={showImages}
             setShowImages={setShowImages}
+            setSelectedImageId={setSelectedImageId}
           />
         </div>
 
